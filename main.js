a1bff69a33ce8fb9b246c5ed7a96f77ba5447414
l3miage-mbayamh <Hippolyte-Djecomde.Mbayam@etu.univ-grenoble-alpes.fr>
2025-12-09 21:06:53 +0100

    bulletin done

--- main.js
+++ main.js
@@ -1,19 +1,311 @@
+import { Bulletin, Evaluation } from "./modules/evaluation.js";
 import { Cours } from "./modules/Pedagogie.js";
 import { Eleve,Enseignant } from "./modules/Personnes.js";
 import { Etablissement,Classe } from "./modules/Structure.js";
 
 
-let eleveTest = {
-    nom : "Djegomde",
-    prenom : "Hippolyte",
-    sexe : "M",
-    date_naiss:"18/12/1996",
-    lieu_naiss : "Ndjamena"
-}
-let eleve1 = new Eleve(eleveTest);
-console.log(eleve1);
 
-let enstes = {
+
+
+// Liste des Eleves
+const listeEleve  = [
+    {
+        nom: "Diallo",
+        prenom: "Aminata",
+        sexe: "F",
+        date_naiss: "2007-03-15",
+        lieu_naiss: "N'Djamena"
+    },
+    {
+        nom: "Ngaryo",
+        prenom: "Brahim",
+        sexe: "M",
+        date_naiss: "2006-11-02",
+        lieu_naiss: "Moundou"
+    },
+    {
+        nom: "Tchoumba",
+        prenom: "Mariam",
+        sexe: "F",
+        date_naiss: "2008-06-21",
+        lieu_naiss: "Sarh"
+    },
+    {
+        nom: "Oumar",
+        prenom: "Ismaël",
+        sexe: "M",
+        date_naiss: "2007-01-10",
+        lieu_naiss: "Abéché"
+    },
+    {
+        nom: "Kassir",
+        prenom: "Sara",
+        sexe: "F",
+        date_naiss: "2006-09-30",
+        lieu_naiss: "Bongor"
+    }
+];
+
+// Listes de Cours 
+const matieres = [
+    {
+        nomMatiere: "Mathématiques",
+        domaine: "Sciences",
+        coefficient: 5,
+        classe: "2nd",
+        enseignant: "Mme Dupont"
+    },
+    {
+        nomMatiere: "Physique-Chimie",
+        domaine: "Sciences",
+        coefficient: 4,
+        classe: "1ère S",
+        enseignant: "M. Leroy"
+    },
+    {
+        nomMatiere: "Histoire-Géographie",
+        domaine: "Lettres & Sciences humaines",
+        coefficient: 3,
+        classe: "Terminale",
+        enseignant: "Mme Bernard"
+    },
+    {
+        nomMatiere: "Anglais",
+        domaine: "Langues",
+        coefficient: 2,
+        classe: "2nd",
+        enseignant: "M. Johnson"
+    },
+    {
+        nomMatiere: "Informatique",
+        domaine: "Technologie",
+        coefficient: 4,
+        classe: "1ère STI2D",
+        enseignant: "Mme Fontaine"
+    }
+];
+// Liste des evalautions 
+const evaluations = [
+    // --- Élève : Aminata Diallo (4 évaluations) ---
+    {
+        valeur: 14,
+        type: "Devoir",
+        date: "2024-10-05",
+        cours: {
+            nomMatiere: "Mathématiques",
+            domaine: "Sciences",
+            coefficient: 5,
+            classe: "2nd",
+            enseignant: "Mme Dupont"
+        },
+        eleve: {
+            nom: "Diallo",
+            prenom: "Aminata",
+            sexe: "F",
+            date_naiss: "2007-03-15",
+            lieu_naiss: "N'Djamena"
+        }
+    },
+    {
+        valeur: 16,
+        type: "Devoir",
+        date: "2024-11-12",
+        cours: {
+            nomMatiere: "Anglais",
+            domaine: "Langues",
+            coefficient: 2,
+            classe: "2nd",
+            enseignant: "M. Johnson"
+        },
+        eleve: {
+            nom: "Diallo",
+            prenom: "Aminata",
+            sexe: "F",
+            date_naiss: "2007-03-15",
+            lieu_naiss: "N'Djamena"
+        }
+    },
+    {
+        valeur: 12,
+        type: "Composition",
+        date: "2024-12-01",
+        cours: {
+            nomMatiere: "Anglais",
+            domaine: "Langues",
+            coefficient: 2,
+            classe: "1ère S",
+            enseignant: "M. Leroy"
+        },
+        eleve: {
+            nom: "Diallo",
+            prenom: "Aminata",
+            sexe: "F",
+            date_naiss: "2007-03-15",
+            lieu_naiss: "N'Djamena"
+        }
+    },
+    {
+        valeur: 15,
+        type: "Composition",
+        date: "2024-12-18",
+        cours: {
+            nomMatiere: "Mathématiques",
+            domaine: "Sciences",
+            coefficient: 5,
+            classe: "2nd",
+            enseignant: "Mme Dupont"
+        },
+        eleve: {
+            nom: "Diallo",
+            prenom: "Aminata",
+            sexe: "F",
+            date_naiss: "2007-03-15",
+            lieu_naiss: "N'Djamena"
+        }
+    },
+
+    // --- Élève : Brahim Ngaryo ---
+    {
+        valeur: 10,
+        type: "Devoir",
+        date: "2024-10-09",
+        cours: {
+            nomMatiere: "Informatique",
+            domaine: "Technologie",
+            coefficient: 4,
+            classe: "1ère STI2D",
+            enseignant: "Mme Fontaine"
+        },
+        eleve: {
+            nom: "Ngaryo",
+            prenom: "Brahim",
+            sexe: "M",
+            date_naiss: "2006-11-02",
+            lieu_naiss: "Moundou"
+        }
+    },
+
+    // --- Élève : Mariam Tchoumba ---
+    {
+        valeur: 13,
+        type: "composition",
+        date: "2024-11-28",
+        cours: {
+            nomMatiere: "Histoire-Géographie",
+            domaine: "Lettres & Sciences humaines",
+            coefficient: 3,
+            classe: "Terminale",
+            enseignant: "Mme Bernard"
+        },
+        eleve: {
+            nom: "Tchoumba",
+            prenom: "Mariam",
+            sexe: "F",
+            date_naiss: "2008-06-21",
+            lieu_naiss: "Sarh"
+        }
+    },
+
+    // --- Élève : Ismaël Oumar ---
+    {
+        valeur: 17,
+        type: "devoir",
+        date: "2024-10-20",
+        cours: {
+            nomMatiere: "Mathématiques",
+            domaine: "Sciences",
+            coefficient: 5,
+            classe: "2nd",
+            enseignant: "Mme Dupont"
+        },
+        eleve: {
+            nom: "Oumar",
+            prenom: "Ismaël",
+            sexe: "M",
+            date_naiss: "2007-01-10",
+            lieu_naiss: "Abéché"
+        }
+    },
+
+    // --- Élève : Sara Kassir ---
+    {
+        valeur: 9,
+        type: "devoir",
+        date: "2024-09-18",
+        cours: {
+            nomMatiere: "Anglais",
+            domaine: "Langues",
+            coefficient: 2,
+            classe: "2nd",
+            enseignant: "M. Johnson"
+        },
+        eleve: {
+            nom: "Kassir",
+            prenom: "Sara",
+            sexe: "F",
+            date_naiss: "2006-09-30",
+            lieu_naiss: "Bongor"
+        }
+    },
+    {
+        valeur: 11,
+        type: "composition",
+        date: "2024-12-10",
+        cours: {
+            nomMatiere: "Informatique",
+            domaine: "Technologie",
+            coefficient: 4,
+            classe: "1ère STI2D",
+            enseignant: "Mme Fontaine"
+        },
+        eleve: {
+            nom: "Kassir",
+            prenom: "Sara",
+            sexe: "F",
+            date_naiss: "2006-09-30",
+            lieu_naiss: "Bongor"
+        }
+    },
+
+    // --- Encore Brahim ---
+    {
+        valeur: 14,
+        type: "composition",
+        date: "2024-12-15",
+        cours: {
+            nomMatiere: "Physique-Chimie",
+            domaine: "Sciences",
+            coefficient: 4,
+            classe: "1ère S",
+            enseignant: "M. Leroy"
+        },
+        eleve: {
+            nom: "Ngaryo",
+            prenom: "Brahim",
+            sexe: "M",
+            date_naiss: "2006-11-02",
+            lieu_naiss: "Moundou"
+        }
+    }
+];
+
+const eleveAminata = {
+ 
+            nom: "Diallo",
+            prenom: "Aminata",
+            sexe: "F",
+            date_naiss: "2007-03-15",
+            lieu_naiss: "N'Djamena"
+    
+}
+let eleve1 = new Eleve(eleveAminata);
+
+
+let bulletin1 = new Bulletin(1,eleve1,evaluations);
+console.log(bulletin1);
+console.log(bulletin1. moyenne());
+
+/*let enstes = {
     nom : "Djegomde",
     prenom : "Hippolyte",
     sexe : "M",
@@ -47,5 +339,8 @@ console.log(etab1)
 
 let enseignant = new Enseignant(enstes)
 let cours1 = new Cours("Mathématique","scientifique",4,cl1,enseignant);
-console.log(cours1.description);
+//console.log(cours1.description);
 
+
+let evaluation = new Evaluation(15,"devoir","12/12/2025",cours1,eleve1);
+console.log(evaluation.description);*/
--- modules/Evaluation.js
+++ modules/Evaluation.js
@@ -1,27 +1,143 @@
-import { Cours } from "./Pedagogie";
-import { Eleve } from "./Personnes";
-
 export class Evaluation {
-    constructor(valeur,type,date,appreciation) {
+    constructor(valeur,type,date,cours,eleve) {
         this._valeur = valeur;
         this._type = type;
         this._dateEvaluation = date;
-        this._appreciation = appreciation;
-        this.cours = Cours;
-        this.eleve = Eleve;
+        this.cours = cours;
+        this.eleve = eleve;
     }
+
+      get observation() {
+        const note = this._valeur;
+        switch (true) {
+            case (note < 0 || note > 20): return "Note invalide";
+            case (note <= 5):  return "Nulle";
+            case (note <= 7):  return "Médiocre";
+            case (note < 10):  return "Insuffisant";
+            case (note < 11):  return "Moyen";     
+            case (note < 12):  return "Passable"; 
+            case (note < 14):  return "Assez-Bien";
+            case (note < 16):  return "Bien";
+            case (note < 19):  return "Très-Bien";
+            case (note <= 20): return "Excellent";
+            default: return "Non noté";
+        }
+    }
+
+    
+    get description(){
+        return `Evaluation :  
+          Type : ${this._type}\n
+          Note obtenue = ${this._valeur}/20 => Observation : ${this.observation}\n
+          Date :  ${this._dateEvaluation}\n
+          Cours : ${this.cours.nomMatiere}\n
+          Eleve : ${this.eleve.nom}\n
+          Classe : ${this.cours.classe.nom}\n
+          `;
+    }
+
 }
 
 export class Bulletin {
-    constructor(eleve,trimestre,ensembleNotes) {
+    constructor(trimestre,eleve,ensembleNotes) {
         this._trimestre = trimestre;
-        this._moyenne = moyenne;
-        this._appreciationProf = appreciationProf;
-        this._appreciationConseil = appreciationConseil;
-        this._sanction = sanction;
-        this._distinction = distinction;
+        this.eleve = eleve;
+        this.ensembleNotes = ensembleNotes.filter(item =>  item.eleve.prenom===this.eleve.prenom && item.eleve.nom===this.eleve.nom);
+    }
+   
+    tabDiscipline(){
+        let tabDiscipline = [];
+         for (const key in this.ensembleNotes) {
+         tabDiscipline .push(this.ensembleNotes[key].cours.nomMatiere);
+        }
+        return tabDiscipline;
 
     }
+
+    tabCoef(){
+        let tabcoefs = [];
+        for (const key in this.ensembleNotes) {
+         tabcoefs.push(this.ensembleNotes[key].cours.coefficient);
+        }
+        return tabcoefs;
+    }
+
+    tabMoyenDevoir(){
+        let tabMoyDev = [];
+        for (const key in this.ensembleNotes) {
+            if (this.ensembleNotes[key].type==="Devoir") {
+                tabMoyDev.push(this.ensembleNotes[key].valeur);
+            }
+            
+        }
+
+        return tabMoyDev;
+    }
+
+      tabMoyenCompo(){
+        let tabMoyCompo = [];
+        for (const key in this.ensembleNotes) {
+            if (this.ensembleNotes[key].type==="Composition") {
+                tabMoyCompo.push(this.ensembleNotes[key].valeur);
+            }
+            
+        }
+
+        return tabMoyCompo;
+    }
+
+
+   
+    tabMoyenneDes2(){
+        let tabMoyDes2 = [];
+        let tab1 = this.tabMoyenDevoir();
+        let tab2 = this.tabMoyenCompo();
+        for (let index1 = 0; index1 < tab1.length; index1++) {
+          
+            for (let index2 = 0; index2 < tab2.length; index2++) {
+                if (index1===index2) {
+                    let moy = (tab1[index1]+tab2[index2])/2;
+                    tabMoyDes2.push(moy);
+                }
+                
+            }
+            
+        }
+
+        return tabMoyDes2;
+    }
+
+
+    tabMoyenneCoef(){
+        let tabMoyCoef = [];
+        let tab1 = this.tabCoef();
+        let tab2 =  this.tabMoyenneDes2();
+
+        for (let index1 = 0; index1 < tab1.length; index1++) {
+          
+            for (let index2 = 0; index2 < tab2.length; index2++) {
+                
+                if (index1===index2) {
+                    let moy = tab1[index1]*tab2[index2];
+                     tabMoyCoef.push(moy);
+                }
+
+               
+            }
+            
+        }
+        return tabMoyCoef;
+    }
+
+
+    sommeCoef = ()=>this.tabCoef().reduce((acc, current) =>  acc+current, 0);
+    sommeMoyCoef = ()=>this.tabMoyenneCoef().reduce((acc,current)=>acc+current,0);
+
+    moyenne(){
+        return this.sommeMoyCoef()/this.sommeCoef();
+    }
+
+
 }
 
 
